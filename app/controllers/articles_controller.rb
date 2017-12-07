ef77e5741e332c2d7265554006bd6f764ecc3677
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-12-07 15:05:22 +0000

    Added create method to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,17 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash[:sucess] = 'Article has been created'
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    #whitelist article fields
+    params.require(:article).permit(:title, :body)
+  end
 end
