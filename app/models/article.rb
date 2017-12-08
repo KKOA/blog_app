ec6c72d09a9ab6bb30f68870cd329e42c33fd0df
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-12-08 17:09:45 +0000

    Specified the order in which data is retrieved from articles table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+  default_scope { order(created_at: :desc) }
+  # Specify order in which data picked from database
+  # desc for reverse order
 end
