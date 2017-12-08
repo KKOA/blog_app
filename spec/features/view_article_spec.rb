021a7777c6c788d4e6aedce7c3187287df25671d
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-12-08 17:39:54 +0000

    Updated test to check body character length

--- spec/features/view_article_spec.rb
+++ spec/features/view_article_spec.rb
@@ -3,7 +3,7 @@ RSpec.feature 'Listing articles' do
   before do
     @article1 = Article.create(
       title: 'The first article',
-      body: 'Lorem ipsum dolor sit amet, consectetur.'
+      body: 'Lorem ipsum dolor sit amet, consectetur.'*20
     )
     @article2 = Article.create(
       title: 'The second article',
@@ -22,6 +22,6 @@ RSpec.feature 'Listing articles' do
     expect(@article1.title).to_not appear_before(@article2.title)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
-    expect(@article2.body).to appear_before(@article1.body)
+    expect(@article2.body[0...497]).to appear_before(@article1.body[0...497])
   end
 end
