d3d12b167d886a59bc1c4fa2f60c703d8b4161d6
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-12-09 15:01:59 +0000

    Added edit method to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   protected
 
     def rescourse_not_found
