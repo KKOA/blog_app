322bcaa28ce5ac2ff01fe0f81b441bd7005c38a6
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-12-09 13:43:56 +0000

    Added method rescourse_not_found to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :rescourse_not_found
+
+  protected
+
+    def rescourse_not_found
+    end
 end
