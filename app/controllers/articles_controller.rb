6443de9747810e13eb693d81fda272065ba012a7
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-12-10 09:58:30 +0000

    Refactored article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  # Method executed before the following [methods]
+
   def index
     @articles = Article.all
   end
@@ -19,16 +23,11 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show;end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit;end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:success] = 'Article has been updated'
       redirect_to @article
@@ -39,7 +38,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if  @article.destroy
       flash[:success] = 'Article has been Deleted'
       redirect_to articles_path
@@ -60,4 +58,9 @@ class ArticlesController < ApplicationController
     # whitelist article fields
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
